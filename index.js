5b81c498f04f322e2cb690f2ac50c80fcccc02a6
paul <paul.t.jensen@gmail.com>
2018-12-11 21:24:05 -0600

    Initial commit.

---
+++ index.js
@@ -0,0 +1,17 @@
+/* DynamoDb Data Access
+ * index.js
+ *
+ * Purpose: Provides example functions for DynamoDb using the AWS SDK.
+ * Author: Paul Jensen (paul.t.jensen@gmail.com)
+ */
+
+(function() {
+    'use strict';
+
+    let Aws = require('aws-sdk');
+
+    let dynamodb = {};
+    
+    module.exports = {database: dynamodb};
+
+})();
